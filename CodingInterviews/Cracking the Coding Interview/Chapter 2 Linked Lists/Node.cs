9155a9967ccea617b2644cf2b92b6861a9a4a196
Heyyan <heyyan@gmail.com>
2019-04-20 13:02:10 +0500

    fixed spelling

---
+++ CodingInterviews/Cracking the Coding Interview/Chapter 2 Linked Lists/Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews.Cracking_the_Coding_Interview.Chapter_2_Linked_Lists
+{
+    public class Node
+    {
+        Node next = null;
+        int data;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+        void appendToTail(int d)
+        {
+            Node end = new Node(d);
+            Node n = this;
+            while (n.next != null)
+            {
+                n = n.next;
+            }
+            n.next = end;
+        }
+
+        Node deleteNode(Node head, int d)
+        {
+            Node n = head;
+            if(n.data == d)
+            {
+                return head.next; // move head
+            }
+            while (n.next != null)
+            {
+                if(n.next.data == d)
+                {
+                    n.next = n.next.next;
+                    return head;// head didnt change
+                }
+                n = n.next;
+            }
+            return head;
+        }
+    }
+}
